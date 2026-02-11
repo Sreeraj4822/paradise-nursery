402e65bfac26a316c70d51dcf58eb5b9b4bdc71d
Sreeraj4822 <sreerajrajesh4822@gmail.com>
2026-02-11 12:36:18 -0500

    Updated App.jsx with landing page and Get Started button

--- src/App.jsx
+++ src/App.jsx
@@ -5,63 +5,26 @@ import AboutUs from "./AboutUs";
 import "./App.css";
 
 function App() {
-  const [showLanding, setShowLanding] = useState(true);
   const [showProducts, setShowProducts] = useState(false);
-  const [showCart, setShowCart] = useState(false);
 
   const handleGetStarted = () => {
-    setShowLanding(false);
     setShowProducts(true);
-    setShowCart(false);
-  };
-
-  const handleShowProducts = () => {
-    setShowLanding(false);
-    setShowProducts(true);
-    setShowCart(false);
-  };
-
-  const handleShowCart = () => {
-    setShowLanding(false);
-    setShowProducts(false);
-    setShowCart(true);
   };
 
   return (
     <div>
-      {/* NAVBAR */}
-      {!showLanding && (
-        <nav className="navbar">
-          <h2 onClick={handleShowProducts} style={{ cursor: "pointer" }}>
-            Paradise Nursery
-          </h2>
-          <div>
-            <button onClick={handleShowProducts}>Plants</button>
-            <button onClick={handleShowCart}>Cart</button>
-          </div>
-        </nav>
-      )}
-
-      {/* LANDING PAGE */}
-      {showLanding && (
+      {!showProducts && (
         <div className="background-image">
           <h1>Welcome to Paradise Nursery</h1>
           <p>
-            We bring nature closer to your home with a wide variety of indoor
-            and decorative plants.
+            We offer a wide variety of beautiful indoor plants to brighten your home.
           </p>
           <button onClick={handleGetStarted}>Get Started</button>
+          <AboutUs />
         </div>
       )}
 
-      {/* PRODUCT PAGE */}
       {showProducts && <ProductList />}
-
-      {/* CART PAGE */}
-      {showCart && <CartItem onContinueShopping={handleShowProducts} />}
-
-      {/* ABOUT US SECTION */}
-      {showLanding && <AboutUs />}
     </div>
   );
 }
